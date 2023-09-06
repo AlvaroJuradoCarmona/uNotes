c6109bd60a527bd070bb5e0caad073834b11a188
Alvaro Jurado <quickalvaro@gmail.com>
2023-09-06 22:21:15 +0200

    Fix sql subject list count

--- backend/src/controllers/subject.controller.js
+++ backend/src/controllers/subject.controller.js
@@ -32,7 +32,11 @@ const getSubjectsByFacultyId = async (req,res) => {
         const connection = await getConnection();
         const {idFaculty} = req.params;
 
-        const query = await connection.query("SELECT * FROM subjects WHERE idFaculty = ?", idFaculty);
+        const query = await connection.query(`SELECT s.name, COUNT(d.idSubject) as documentCount FROM subjects s 
+                                                LEFT JOIN documents d ON s.idSubject=d.idSubject 
+                                                WHERE s.idFaculty = ? 
+                                                GROUP BY s.idSubject, s.name 
+                                                ORDER BY s.course, s.name;`, idFaculty);
         
         res.json(query);
     }catch(error){
