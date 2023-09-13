4117b6092f15a17177fc34dfc34996b8c5449035
Alvaro Jurado <quickalvaro@gmail.com>
2023-09-13 17:25:20 +0200

    upload document backend done

--- backend/src/routes/file.routes.js
+++ backend/src/routes/file.routes.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 const router = Router();
 
 import { methods as fileCtrl } from "../controllers/file.controller";
+
 router.get('/', fileCtrl.getFiles)
+router.post('/', fileCtrl.addFile)
 
 export default router;
