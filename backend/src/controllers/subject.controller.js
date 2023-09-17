9731a70e83b23a31fb52911f775484c0437e03a3
Alvaro Jurado <quickalvaro@gmail.com>
2023-09-17 02:10:09 +0200

    add course to query

--- backend/src/controllers/subject.controller.js
+++ backend/src/controllers/subject.controller.js
@@ -32,7 +32,7 @@ const getSubjectsByFacultyId = async (req,res) => {
         const connection = await getConnection();
         const {idFaculty} = req.params;
 
-        const query = await connection.query(`SELECT s.idSubject, s.name, COUNT(d.idSubject) as documentCount FROM subjects s 
+        const query = await connection.query(`SELECT s.idSubject, s.name, s.course, COUNT(d.idSubject) as documentCount FROM subjects s 
                                                 LEFT JOIN documents d ON s.idSubject=d.idSubject 
                                                 WHERE s.idFaculty = ? 
                                                 GROUP BY s.idSubject, s.name 
