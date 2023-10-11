c615814315acd5e5357ae303f4c58de55f3b3730
Alvaro Jurado <quickalvaro@gmail.com>
2023-10-11 19:08:06 +0200

    licence info done

--- frontend/src/Components/subjects/modals/licenseInfo.js
+++ frontend/src/Components/subjects/modals/licenseInfo.js
@@ -4,6 +4,13 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
+import ccby from './../../../assets/licenses/cc_by.png'
+import ccbysa from './../../../assets/licenses/cc_by-sa.png'
+import ccbynd from './../../../assets/licenses/cc_by-nd.png'
+import ccbync from './../../../assets/licenses/cc_by-nc.png'
+import ccbyncsa from './../../../assets/licenses/cc_by-nc-sa.png'
+import ccbyncnd from './../../../assets/licenses/cc_by-nc-nd.png'
+
 import "./../subjects.css";
 
 const style = {
@@ -38,26 +45,47 @@ export default function BasicModal() {
             Información acerca de las licencias
           </Typography>
           <br/>
-          <p><b>Reconocimiento CC BY</b></p>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento CC BY</b></p>
+            <img src={ccby} alt="CC BY" style={{ width: "70px", height: "auto" }}/>
+          </div>
           <p>Esta licencia permite a otros distribuir, remezclar, adaptar y desarrollar su trabajo, incluso comercialmente, siempre que le den crédito por la creación original. Esta es la licencia más flexible que se ofrece. Recomendado para máxima difusión y uso de materiales licenciados.</p>
           <br/>
-          <p><b>Reconocimiento-ShareEqual CC BY-SA</b></p>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento-ShareEqual CC BY-SA</b></p>
+            <img src={ccbysa} alt="CC BY-SA" style={{ width: "70px", height: "auto" }}/>
+          </div>
           <p>Esta licencia permite a otros remezclar, adaptar y desarrollar su trabajo incluso con fines comerciales, siempre que le den crédito y concedan licencias para sus nuevas creaciones bajo los mismos términos. Esta licencia a menudo se compara con las licencias de software libre y de código abierto “copyleft”.</p>
-          <br/>     
-          <p><b>Reconocimiento-SinTrabajoDerivado CC BY-ND</b></p>
+          <br/>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento-SinTrabajoDerivado CC BY-ND</b></p>
+            <img src={ccbynd} alt="CC BY-ND" style={{ width: "70px", height: "auto" }}/>
+          </div>
           <p>Esta licencia permite a otros reutilizar la obra para cualquier fin, incluso comercial; sin embargo, no se puede compartir con otros en forma adaptada y se le debe proporcionar crédito.</p>
-          <br/>       
-          <p><b>Reconocimiento-NoComercial CC BY-NC</b></p>
+          <br/>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento-NoComercial CC BY-NC</b></p>
+            <img src={ccbync} alt="CC BY-NC" style={{ width: "70px", height: "auto" }}/>
+          </div>
           <p>Esta licencia permite a otros remezclar, adaptar y desarrollar su trabajo sin fines comerciales, y aunque sus nuevos trabajos también deben reconocerlo y no ser comerciales, no tienen que licenciar sus trabajos derivados en los mismos términos.</p>
-          <br/>      
-          <p><b>Reconocimiento-NoComercial-CompartirIgual CC BY-NC-SA</b></p>
+          <br/>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento-NoComercial-CompartirIgual CC BY-NC-SA</b></p>
+            <img src={ccbyncsa} alt="CC BY-NC-SA" style={{ width: "70px", height: "auto" }}/>
+          </div>
           <p>Esta licencia permite a otros remezclar, adaptar y desarrollar su trabajo sin fines comerciales, siempre y cuando le den crédito y concedan licencias para sus nuevas creaciones bajo los mismos términos.</p>
-          <br/>      
-          <p><b>Reconocimiento-NoComercial-SinObraDerivada CC BY-NC-ND</b></p>
+          <br/>
+          <div style={{ display: "flex", alignItems: "center", marginBottom: "5px" }}>
+            <p style={{ marginRight: "15px" }}><b>Reconocimiento-NoComercial-SinObraDerivada CC BY-NC-ND</b></p>
+            <img src={ccbyncnd} alt="CC BY-NC-ND" style={{ width: "70px", height: "auto" }}/>
+          </div> 
           <p>Esta licencia es la más restrictiva de las seis licencias principales, sólo permite que otros puedan descargar las obras y compartirlas con otras personas, siempre que se reconozca su autoría, pero no se pueden cambiar de ninguna manera ni se pueden utilizar comercialmente.</p>
           <br/>
-
-          <Button variant="contained" id="doneButton" onClick={handleClose}>Entendido</Button>
+          
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Button variant="contained" id="doneButton" onClick={handleClose}>Entendido</Button>
+            <Button target="_blank" rel="noreferrer" href="https://creativecommons.org/share-your-work/cclicenses/">¿Más información?</Button>
+          </div>
         </Box>
       </Modal>
     </div>
